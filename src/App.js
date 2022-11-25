3c93c5cf48725853ff2a423fd1a2593d2fd8fb29
Jawid <41361157+infjay@users.noreply.github.com>
2022-11-25 20:43:41 +0100

    AppBar & dark theme added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import React from 'react'
 import {Route,Routes} from "react-router-dom";
 import { Box } from '@mui/material';
 import ExerciseDetail from './pages/ExerciseDetail';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import './App.css';
 import { AddBox } from '@mui/icons-material';
+import ResponsiveAppBar from "./components/appBar";
+import Navbar from "./components/Navbar";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 
 const App = () => {
   return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
     <Box width="400px" sx={{width: { xl: '1448px' }}} m="auto">
-        <Navbar />
+        <ResponsiveAppBar />
          <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/exercise/:id" element={<ExerciseDetail />}/>
          </Routes>
          <Footer />
     </Box>
+    </ThemeProvider>
   )
 }
 
