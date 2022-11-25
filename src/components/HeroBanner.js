31ad32dc6411b8af0d3eb697f11358298d4e801a
infjay <jawid.nm@gmail.com>
2022-11-25 09:54:28 +0100

    minimal adjustments

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -11,13 +11,15 @@ const HeroBanner = () => {
         fontWeight="600" fontSize="26px">
             Fitness Club
         </Typography>
-        <Typography fontWeight="700" sx={{ fontSize: { lg:'44px', xs: '40px'}}}>
+        <Typography fontWeight="700" sx={{ fontSize: { lg:'44px', xs: '40px'}}}
+        mb="23px" mt="30px">
             Sweat, Smile <br /> and Repeat
         </Typography>
-        <Typography fontSize="22px" lineHeight="35px" mb={3}>
+        <Typography fontSize="22px" lineHeight="35px" mb={4}>
             Check out the superhuman Exercises
         </Typography>
-        <Button variant="contained" color="error" href='#exercises'>EXPLORE EXERCISES</Button>
+        <Button variant="contained" color="error" href='#exercises' 
+        sx={{ backgroundColor:'#ff2526',padding:'10px'}}>EXPLORE EXERCISES</Button>
         <Typography fontWeight={600}
         color="#ff2625"
         sx={{
