c154dc67230d811bc498337247b15593aff879ad
infjay <jawid.nm@gmail.com>
2022-11-25 09:45:53 +0100

    image and background fixed

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -6,7 +6,7 @@ const HeroBanner = () => {
     <Box sx={{
         mt: {lg:"212px" , xs:"70px"},
         ml: {sm:"50px"}
-    }}>
+    }} position="relative" p="20px">
         <Typography color="#FF2625"
         fontWeight="600" fontSize="26px">
             Fitness Club
@@ -17,8 +17,17 @@ const HeroBanner = () => {
         <Typography fontSize="22px" lineHeight="35px" mb={3}>
             Check out the superhuman Exercises
         </Typography>
-        <Button variant='contained' color="error" href='#exercises'>EXPLORE EXERCISES</Button>
-        <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+        <Button variant="contained" color="error" href='#exercises'>EXPLORE EXERCISES</Button>
+        <Typography fontWeight={600}
+        color="#ff2625"
+        sx={{
+            opacity: 0.1,
+            display:{ lg: 'block',xs:'none' }
+        }}
+        fontSize="200px"
+        >Exercise</Typography>
+        <img src={HeroBannerImage} alt="banner" 
+        className="hero-banner-img"/>
     </Box>
   )
 }
