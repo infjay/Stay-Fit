fccead4136fe5a90b5ebe1feadfad9f8b48986f3
Jawid <41361157+infjay@users.noreply.github.com>
2022-12-01 11:42:07 +0100

    body partr props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,8 @@ import HeroBanner from '../components/HeroBanner'
 
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all")
   return (
     <Box>
       <HeroBanner />
