a1180deca31e83c287f0789b83e541565e1e1d08
Jawid <41361157+infjay@users.noreply.github.com>
2022-12-02 18:31:16 +0100

    minor changes

--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -1,15 +1,14 @@
-import React, { useContext } from 'react';
+import React,{useContext} from 'react'
+import { Box ,Typography} from '@mui/material';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import { Box, Typography } from '@mui/material';
+import BodyPart from './BodyPart'
 
-import ExerciseCard from './ExerciseCard';
-import BodyPart from './BodyPart';
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
-
+  
   return (
     <Typography onClick={() => scrollPrev()} className="right-arrow">
       <img src={LeftArrowIcon} alt="right-arrow" />
@@ -19,7 +18,7 @@ const LeftArrow = () => {
 
 const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
-
+    
   return (
     <Typography onClick={() => scrollNext()} className="left-arrow">
       <img src={RightArrowIcon} alt="right-arrow" />
@@ -27,19 +26,26 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-    {data.map((item) => (
+const HorizontalScrollBar = ({ data , bodyPart, setBodyPart }) => {
+  
+  return (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>           
+    {data.map((item)=> (
       <Box
         key={item.id || item}
-        itemID={item.id || item}
+        itemId={item.id || item}
         title={item.id || item}
         m="0 40px"
-      >
-        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
-      </Box>
-    ))}
-  </ScrollMenu>
-);
+        >
+        <BodyPart item={item} 
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart}/>
+        </Box>
+        )
+        )}
+  </ScrollMenu>    
+  )
+}
 
-export default HorizontalScrollbar;
+
+export default HorizontalScrollBar
