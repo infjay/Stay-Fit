65b7a83c121dc24866de4798477487a0470da798
Jawid <41361157+infjay@users.noreply.github.com>
2022-12-02 17:45:54 +0100

    minor

--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -27,26 +27,19 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollBar = ({ data , bodyPart, setBodyPart }) => {
-  
-  return (
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>           
-    {data.map((item)=> (
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((item) => (
       <Box
         key={item.id || item}
         itemID={item.id || item}
         title={item.id || item}
         m="0 40px"
-        >
-        <BodyPart item={item} 
-        bodyPart={bodyPart} 
-        setBodyPart={setBodyPart}/>
-        </Box>
-        )
-        )}
-  </ScrollMenu>    
-  )
-}
+      >
+        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
+      </Box>
+    ))}
+  </ScrollMenu>
+);
 
-
-export default HorizontalScrollBar
+export default HorizontalScrollbar;
