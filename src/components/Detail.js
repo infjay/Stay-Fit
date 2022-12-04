b6849bc825b3014cdc64712c1b18e24f6c7117bf
Jawid <41361157+infjay@users.noreply.github.com>
2022-12-04 18:37:48 +0100

    Detail description added + targeted muscle icons added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -6,9 +6,38 @@ import EquipmentImage from '../assets/icons/equipment.png'
 
 const Detail = ({ exerciseDetail }) => {
     const {bodyPart , gifUrl, name, target, equipment} = exerciseDetail;
-  return (
+    const extraDetail = [
+      {icon: BodyPartImage,
+      name: bodyPart},
+      {icon: TargetImage,
+      name: target},
+      {icon: EquipmentImage,
+      name: bodyPart},
+    ]
+  
+    return (
     <Stack gap="60px" sx={{flexDirection: {lg:'row' , p:"20px",alignItems:'center'}}}>
         <img src={gifUrl} alt={name} className="detail-image" loading='lazy' />
+      <Stack sx={{ gap: { lg:'35px', xs:'20px'}}}>
+      <Typography variant='h3'>
+        {name}
+      </Typography>
+      <Typography variant='h6'>
+        Exercises keep you strong. {name} is one of the
+        best exercises to target your {target}.
+        it will improve you mood and gain energy.
+      </Typography>
+      {extraDetail.map((item) => (
+        <Stack key={item.name} direction="row" gap="24px" alignItems="center" >
+          <Button sx={{ background:'#ffa',borderRadius:'50%', width:'100px',height:'100px'}}>
+            <img src={item.icon} alt={bodyPart} />
+          </Button>
+          <Typography variant='h5' color="red" textTransform="capitalize">
+            {item.name}
+          </Typography>
+        </Stack>
+      ))}
+      </Stack>
     </Stack>
   )
 }
