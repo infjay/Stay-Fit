580b9d61808add4d89c3947481976a835e2de134
Jawid <41361157+infjay@users.noreply.github.com>
2022-12-04 18:37:16 +0100

    404 error fixed

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -14,7 +14,8 @@ useEffect(()=> {
   const fetchExercisesData = async ()=> {
   const exerciseDbUrl = "https://exercisedb.p.rapidapi.com"
   const youTubeSearchUrl = "https://youtube-search-and-download.p.rapidapi.com"
-  const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`,exerciseOptions)
+  const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,exerciseOptions)
+  
   setExerciseDetail(exerciseDetailData)
 }
   fetchExercisesData()
